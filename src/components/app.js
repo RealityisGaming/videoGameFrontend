1f2ac124e1c9e518efb9dcc868fe8a9482a7195b
Matthew Hoecker <98118482+RealityisGaming@users.noreply.github.com>
2022-03-14 20:58:26 -0500

    Added folders/files for character/enemy/locations

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import {BrowserRoute as Router, Switch, Route} from 'react-router-dom';
+import Axios from 'axios';
+
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Welcome to Elrawntrec</h1>
       </div>
     );
   }
